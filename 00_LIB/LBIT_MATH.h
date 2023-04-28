c28195ef4e5307f12a8ada9b8a0d9dc0d16189ba
AhmedHammadAli <ahmed.khaled.hammad.ali@gmail.com>
2023-04-28 22:13:00 +0300

    MOD | LBIT_MATH.h and LERROR_STATES.h Files

--- 00_LIB/LBIT_MATH.h
+++ 00_LIB/LBIT_MATH.h
@@ -15,6 +15,15 @@
 /*                    Macros funcitons for Bit operations                 */
 /**************************************************************************/
 
-
+#define SET_BIT(REG , BITNUM)     ((REG) |=  (1U<<(BITNUM)))
+#define CLR_BIT(REG , BITNUM)     ((REG) &= ~(1U<<(BITNUM)))
+#define TOG_BIT(REG , BITNUM)     ((REG) ^=  (1U<<(BITNUM)))
+#define GET_BIT(REG , BITNUM)     ( ((REG) >>(BITNUM))&1U  ) 
+#define SET_BITS(REG , VAL)       ((REG) |= (VAL))
+#define CLR_BITS(REG , MSK)       ((REG) &= (MSK))
+#define TOG_BITS(REG , MSK)       ((REG) ^= (MSK))
+#define SET_ALL_BITS(REG)         ((REG) = (0xFFFFFFFF))
+#define CLR_ALL_BITS(REG)         ((REG) = (0U) )
+#define TOG_ALL_BITS(REG)         ((REG) = ~(REG))
 
 #endif
