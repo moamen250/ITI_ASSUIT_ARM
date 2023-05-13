523933ae1c3a763fbfcda4c8e986695e7af7befc
mina121 <minanabil635@gmail.com>
2023-05-14 01:01:46 +0200

    i set implementation of MSTK_delay_us() api

--- 01_MCAL/02_MSTK/MSTK_Program.c
+++ 01_MCAL/02_MSTK/MSTK_Program.c
@@ -53,8 +53,32 @@ void _delay_ms(u32 Copy_u32Time)
    // Stop Timer
    CLR_BIT(MSTK->CTRL, 0 ) ;
 }
+/******************************************************************************/
+/*                           02_ MGPIO_errSetPinData                          */
+/*----------------------------------------------------------------------------*/
+/* 1- Function Description -> Set Delay by MicroSec---->>_delay_us            */
+/* 2- Function Input       ->                                                 */
+/*                     @param u32 Copy_u32Time                                */
+/*                                                                            */
+/* 3- Function Return      -> void                                            */
+/*                                                                            */
+/******************************************************************************/
+/* Clock = AHB/8 - Disable STK > SYSCLK = 16 MHZ -> STK_Frq = 2 MHZ , 0.5 Micro second */
 void _delay_us(u32 Copy_u32Time)
 {
-//TODO
+  //Initialize timer
+	MSTK_errInit() ;
+   //Disable INT
+   CLR_BIT(MSTK->CTRL, 1 ) ;
+   // LOAD_REG
+   MSTK->LOAD = Copy_u32Time * 2 ;
+   // VAL with any value -> Immediate Reload
+   MSTK->VAL = 0 ;
+   // Enable Timer
+   SET_BIT(MSTK->CTRL, 0 ) ;
+   // Polling
+   while(GET_BIT(MSTK->CTRL,16) == 0) ;
+   // Stop Timer
+   CLR_BIT(MSTK->CTRL, 0 ) ;
 }
 
