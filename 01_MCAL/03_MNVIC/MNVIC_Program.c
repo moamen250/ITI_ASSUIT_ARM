5836add88abbd89a8d7f9a1df7c51f75cce211d0
ElGr8tly <elgr8tly@gmail.com>
2023-05-22 06:46:48 +0300

    ADD | ERROR STATE ES_OUT_OF_RANGE_PRE_INT to ES file in LIB , ADD | ES to 4 API SoftwareSystemPriority & Enable/Disable PerInterrupt & Set/Clear PendingInterrupt

--- 01_MCAL/03_MNVIC/MNVIC_Program.c
+++ 01_MCAL/03_MNVIC/MNVIC_Program.c
@@ -37,26 +37,71 @@
 static MNVIC_SysPriority_t GLB_uddtSysPriority ;
 ES_t MNVIC_errInitSoftwareSystemPriority(MNVIC_SysPriority_t Copy_uddtPriorityOption)
 {
+	ES_t LOC_uddtState = ES_OK;
+	if(Copy_u8IntIndex < MNVIC_INVALID_Priority)
+	{
 	GLB_uddtSysPriority  = Copy_uddtPriorityOption ;
 	//TODO   -> Mariam
+	}
+	else{
+		LOC_uddtState = ES_WRONG_MODE_VALUE ;
+	}
+	return LOC_uddtState;
 }
 
-// Error state -> Yahya
-ES_t MNVIC_errEnablePerInterrupt(u8 Copy_u8IntIndex)  //32
+
+ES_t MNVIC_errEnablePerInterrupt(u8 Copy_u8IntIndex)
 {
-	MNVIC->ISER[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	ES_t LOC_uddtState = ES_OK;
+	if(Copy_u8IntIndex < BITS_PER_INT)
+	{
+		MNVIC->ISER[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	}
+	else
+	{
+		LOC_uddtState = ES_OUT_OF_RANGE_PRE_INT;
+	}
+	return LOC_uddtState;
 }
+
 ES_t MNVIC_errDisablePerInterrupt(u8 Copy_u8IntIndex)
 {
-	MNVIC->ICER[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	ES_t LOC_uddtState = ES_OK;
+	if(Copy_u8IntIndex < BITS_PER_INT)
+	{
+		MNVIC->ICER[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	}
+	else 
+	{
+		LOC_uddtState = ES_OUT_OF_RANGE_PRE_INT;
+	}
+	return LOC_uddtState;
 }
 ES_t MNVIC_errSetPendingInterrupt(u8 Copy_u8IntIndex)
 {
-	MNVIC->ISPR[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	ES_t LOC_uddtState = ES_OK;
+	if(Copy_u8IntIndex < BITS_PER_INT)
+	{
+		MNVIC->ISPR[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	}
+	else 
+	{
+		LOC_uddtState = ES_OUT_OF_RANGE_PRE_INT;
+	}
+	return LOC_uddtState;
 }
 ES_t MNVIC_errClearPeningInterrupt(u8 Copy_u8IntIndex)
 {
-	MNVIC->ICPR[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	ES_t LOC_uddtState = ES_OK;
+	if(Copy_u8IntIndex < BITS_PER_INT)
+	{
+		MNVIC->ICPR[Copy_u8IntIndex / 32]  = (1U << (Copy_u8IntIndex %32) )  ;
+	}
+	else
+	{
+		LOC_uddtState = ES_OUT_OF_RANGE_PRE_INT;
+	}
+	return LOC_uddtState;
 }
 ES_t MNVIC_errIsInterruptActive(u8 Copy_u8IntIndex , u8* Copy_pu8ActiveStatus)
 {
